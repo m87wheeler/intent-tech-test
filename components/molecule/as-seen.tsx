5982837ada0a534b1dcc63d7827b458958b884ce
Martin Wheeler <martin.wheeler@glisser.com>
2023-01-25 12:29:30 +0000

    chore: add responsiveness to as-seen component

--- components/molecule/as-seen.tsx
+++ components/molecule/as-seen.tsx
@@ -9,7 +9,7 @@ const AsSeen = ({ brands = [] }: Props) => {
   return (
     <div className="flex flex-col items-center my-16">
       <Typography className="font-bold text-xs">AS SEEN ON</Typography>
-      <div className="flex gap-10">
+      <div className="flex flex-col md:flex-row md:gap-10">
         {brands.map((br) => (
           <div className="relative w-36 h-20" key={br.id}>
             <Image
